7c203084151d25e29f37c8cccb7b6a3a47c97991
Zedan Saheer <zed.saheer5@gmail.com>
2021-08-10 02:52:15 +0530

    loading rows and api content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 const App = () => {
   return (
     <div>
       <h1>Hello world</h1>
+      <Row title="Netflix Originals" fetchURL={requests.fetchNetflix}/>
+      <Row title="Treding Now" fetchURL={requests.fetchTrendingMovies} />
     </div>
   )
 }
---
+++ src/Row.js
@@ -0,0 +1,25 @@
+import React , {useState , useEffect} from 'react';
+import axios from 'axios';
+
+const row = ({title , fetchURL}) => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            console.log(request);
+            return request;
+        }
+        fetchData();
+    }, [])
+
+    return (
+        <div>
+            {/* title */}
+            <h1>{title}</h1>
+            {/* container -> posters */}
+        </div>
+    )
+}
+
+export default row
